2f317762afd8804553b11200501946f6e0dafa9b
bdonka <dbrodzinska@gmail.com>
2026-03-12 20:04:44 +0100

    Added test controller with GET endpoint

---
+++ MovieRentalSystem.NET.WebApi/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRentalSystem.NET.WebApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class TestController : ControllerBase
+{
+    // GET: api/hello
+    [HttpGet("hello")]
+    public IActionResult GetHello()
+    {
+        return Ok("Hello World!");
+    }
+}
--- MovieRentalSystem.NET.WebApi/Program.cs
+++ MovieRentalSystem.NET.WebApi/Program.cs
@@ -1,7 +1,10 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -10,6 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
@@ -32,6 +36,8 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
+app.MapControllers();
 
 app.Run();
 
