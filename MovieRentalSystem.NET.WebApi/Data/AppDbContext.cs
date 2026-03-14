b5fe5892c44eaa1c5571d940648ddf8512c811a7
bdonka <dbrodzinska@gmail.com>
2026-03-14 18:11:18 +0100

    Added AppDbContext with DbSet definitions for all entities

---
+++ MovieRentalSystem.NET.WebApi/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRentalSystem.NET.WebApi.Entities;
+
+namespace MovieRentalSystem.NET.WebApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Genre> Genres => Set<Genre>();
+    public DbSet<Movie> Movies => Set<Movie>();
+    public DbSet<MovieGenre> MovieGenres => Set<MovieGenre>();
+    public DbSet<MoviePhysicalCopy> MoviePhysicalCopies => Set<MoviePhysicalCopy>();
+    public DbSet<Rental> Rentals => Set<Rental>();
+    public DbSet<User> Users => Set<User>();
+
+}
+
+
