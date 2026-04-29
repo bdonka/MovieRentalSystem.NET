271befea8acd231724e32e1ce7ee44d9ff72a27f
bdonka <dbrodzinska@gmail.com>
2026-04-29 14:38:23 +0200

    Created global error handling middleware, response time middleware and request and response logging middleware. Added configurations in Program.cs and appsettings.json

---
+++ MovieRentalSystem.NET.WebApi/Middlewares/ResponseTimeMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace MovieRentalSystem.NET.WebApi.Middlewares
+{
+    public class ResponseTimeMiddleware : IMiddleware
+    {
+        private readonly ILogger<ResponseTimeMiddleware> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly TimeSpan _defaultThreshold = TimeSpan.FromMilliseconds(500);
+        public ResponseTimeMiddleware(ILogger<ResponseTimeMiddleware> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var watch = Stopwatch.StartNew();
+            await next.Invoke(context);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            var thresholdConfigValue = _configuration.GetValue<string>("Middlewares:RequestTimeWarningThreshold");
+
+            var threshold = TimeSpan.TryParse(thresholdConfigValue, out var parsedThreshold) ? parsedThreshold : _defaultThreshold;
+
+            if (elapsedMs > threshold.TotalMilliseconds)
+            {
+                _logger.LogWarning("Request [{Method}] {Path} took {ElapsedMilliseconds} ms", context.Request.Method, context.Request.Path, elapsedMs);
+            }
+        }
+    }
+}
--- MovieRentalSystem.NET.WebApi/Program.cs
+++ MovieRentalSystem.NET.WebApi/Program.cs
@@ -1,19 +1,29 @@
 using FluentValidation;
+using Microsoft.AspNetCore.HttpLogging;
 using MovieRentalSystem.NET.Application.ApplicationDependencies;
 using MovieRentalSystem.NET.Infrastructure.InfrastructureDependencies;
+using MovieRentalSystem.NET.WebApi.Middlewares;
 using Scalar.AspNetCore;
 using Serilog;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .WriteTo.Console()
-    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.Request | HttpLoggingFields.Response;
+});
 
-builder.Host.UseSerilog();
+//Log.Logger = new LoggerConfiguration()
+//    .ReadFrom.Configuration(builder.Configuration)
+//    .WriteTo.Console()
+//    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+//    .CreateLogger();
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext());
 
 builder.AddServiceDefaults();
 
@@ -21,7 +31,7 @@ builder.Services.AddControllers()
     .AddJsonOptions(o =>
     {
         o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    }); ;
+    });
 
 builder.Services.AddApplicationDependencies();
 
@@ -30,7 +40,27 @@ builder.Services.AddInfrastructureDependencies(builder.Configuration);
 builder.Services.AddOpenApi();
 
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+builder.Services.AddProblemDetails();
+
+builder.Services.AddScoped<ResponseTimeMiddleware>();
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging(options =>
+{
+    options.MessageTemplate =
+        "HTTP {Method} {Path} => {StatusCode} in {Elapsed} ms";
+});
+
+app.UseExceptionHandler();
+
+app.UseMiddleware<RequestResponseLoggingMiddleware>();
+
+app.UseMiddleware<ResponseTimeMiddleware>();
+
 app.MapDefaultEndpoints();
 
 if (app.Environment.IsDevelopment())
